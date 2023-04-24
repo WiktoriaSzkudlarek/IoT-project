f520205f92d0f6435b93aa6c4b30b59ee81623e4
Wiktoria Szkudlarek <wiki.szkudi@gmail.com>
2023-04-24 23:40:19 +0200

    init commit

---
+++ ServiceSdkDemo.Console/ServiceSdkDemo.Console/FeatureSelector.cs
@@ -0,0 +1,72 @@
+﻿
+
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace ServiceSdkDemo.Console
+{
+    internal static class FeatureSelector
+    {
+        public static void PrintMenu()
+        {
+            System.Console.WriteLine(@"
+1 - C2D
+2 - Direct Method
+3 - Device Twin
+0 - Exit");
+        }
+
+        public static async Task Execute(int feature, Lib.IoTHubManager manager) 
+        { 
+            switch(feature)
+            {
+                case 1:
+                    {
+                        System.Console.WriteLine("\nType your message (confirm with enter):");
+                        string messageText = System.Console.ReadLine() ?? string.Empty;
+
+                        System.Console.WriteLine("\nType your device id (confirm with enter):");
+                        string deviceId = System.Console.ReadLine() ?? string.Empty;
+
+                        await manager.SendMessage(messageText, deviceId);
+                    }
+                    break;
+                case 2:
+                    {
+                        System.Console.WriteLine("\nType your device id (confirm with enter):");
+                        string deviceId = System.Console.ReadLine() ?? string.Empty;
+                        try
+                        {
+                            var result = await manager.ExecuteDeviceMethod("SendMessages", deviceId);
+                            System.Console.WriteLine($"Method executed with status {result}");
+                        }
+                        catch(DeviceNotFoundException)
+                        {
+                            System.Console.WriteLine("Device not connected!");
+                        }
+                    }
+                    break;
+                case 3:
+                    {
+                        System.Console.WriteLine("\nType your device id (confirm with enter):");
+                        string deviceId = System.Console.ReadLine() ?? string.Empty;
+
+                        System.Console.WriteLine("\nType your property name (confirm with enter):");
+                        string propertyName = System.Console.ReadLine() ?? string.Empty;
+
+                        var random = new Random();
+                        await manager.UpdateDesiredTwin(deviceId, propertyName, random.Next());
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal static int ReadInput()
+        {
+            var keyPressed = System.Console.ReadKey();
+            var isParsed = int.TryParse(keyPressed.KeyChar.ToString(), out var value);
+            return isParsed ? value : -1;
+        }
+    }
+}
---
+++ ServiceSdkDemo.Console/ServiceSdkDemo.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Devices;
+using ServiceSdkDemo.Console;
+using ServiceSdkDemo.Lib;
+using System;
+using System.IO;
+using System.Text.Json;
+
+string text = File.ReadAllText("config.json");
+var config = JsonSerializer.Deserialize<Config>(text);
+
+using var serviceClient = ServiceClient.CreateFromConnectionString(config.ServiceConnectionString);
+using var registryManager = RegistryManager.CreateFromConnectionString(config.ServiceConnectionString);
+
+var manager = new IoTHubManager(serviceClient, registryManager);
+
+int input;
+do
+{
+    FeatureSelector.PrintMenu();
+    input = FeatureSelector.ReadInput();
+    await FeatureSelector.Execute(input, manager);
+} while (input != 0);
+
